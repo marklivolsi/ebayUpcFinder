e1c4ecb09b5c7b6b2e35b4484fdafe1dde276a86
Mark Livolsi <mark.c.livolsi@gmail.com>
2019-05-28 17:51:41 -0400

    Update test cases

--- src/test/java/com/mcl/utilities/ApiParameterStringBuilderTest.java
+++ src/test/java/com/mcl/utilities/ApiParameterStringBuilderTest.java
@@ -20,7 +20,7 @@ public class ApiParameterStringBuilderTest {
     }
 
     @Test
-    public void testBuildReturnsProperlyFormattedString() {
+    public void testBuildEncodedStringReturnsProperlyFormattedString() {
         ApiParameterStringBuilder builder = new ApiParameterStringBuilder(testParameters);
         String formattedParameters = builder.buildEncodedString();
         String expectedSort = sortString("key+1=val1&key%242=val2&key3=val%5E3");
