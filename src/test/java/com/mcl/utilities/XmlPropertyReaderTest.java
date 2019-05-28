072a18a690382f745918653d2d92b28e82e4af80
Mark Livolsi <mark.c.livolsi@gmail.com>
2019-05-28 13:50:32 -0400

    Update test cases

--- src/test/java/com/mcl/utilities/XmlPropertyReaderTest.java
+++ src/test/java/com/mcl/utilities/XmlPropertyReaderTest.java
@@ -4,21 +4,29 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.security.InvalidParameterException;
 
 public class XmlPropertyReaderTest {
 
     private String xmlFilePath = "src/main/resources/FindingApiParameters.xml";
 
     @Test
-    public void testGetPropertyReturnsCorrectString() {
-        try {
-            XmlPropertyReader reader = new XmlPropertyReader(xmlFilePath);
-            String value = reader.getProperty("FINDING_API_VERSION");
-            Assert.assertEquals("1.13.0", value);
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void testGetPropertyReturnsCorrectString() throws IOException {
+        XmlPropertyReader reader = new XmlPropertyReader(xmlFilePath);
+        String value = reader.getProperty("FINDING_API_VERSION");
+        Assert.assertEquals("1.13.0", value);
     }
+
+    @Test(expected = IOException.class)
+    public void testXmlPropertyReaderThrowsExceptionForInvalidFilepath() throws IOException {
+        XmlPropertyReader reader = new XmlPropertyReader("bogus/filepath");
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    public void testGetPropertyThrowsExceptionForNonexistentKey() throws IOException {
+        XmlPropertyReader reader = new XmlPropertyReader(xmlFilePath);
+        String value = reader.getProperty("bogus-property");
+    }
+
 
 }
