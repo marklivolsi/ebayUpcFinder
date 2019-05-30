fb6a061046f7e73bca22ff678e431b2084f92f32
Mark Livolsi <mark.c.livolsi@gmail.com>
2019-05-30 14:10:45 -0400

    Add ResponseDeserializer class and tests

--- src/main/java/com/mcl/App.java
+++ src/main/java/com/mcl/App.java
@@ -1,6 +1,10 @@
 package com.mcl;
 
+import com.mcl.config.PropertyKeys;
+import com.mcl.requests.Request;
 import com.mcl.utilities.DefaultParameterMapBuilder;
+import com.mcl.utilities.RequestBuilder;
+import com.mcl.utilities.UrlStringBuilder;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,6 +15,12 @@ public class App {
     public static void main(String[] args) throws IOException {
         Map<String, String> params = new DefaultParameterMapBuilder().getDefaultShoppingApiParameterMap();
         System.out.println(params);
+        String baseUrl = PropertyKeys.FIND_API_BASE_URL.getValue();
+        System.out.println(baseUrl);
+        String url = new UrlStringBuilder(baseUrl, params).getUrlString();
+        System.out.println(url);
+        Request req = new RequestBuilder(baseUrl, params).buildRequest();
+        System.out.println(req);
     }
 
 }
