5b348f5d438aeecfeb6f5b995741228de67741a7
Mark Livolsi <mark.c.livolsi@gmail.com>
2019-05-30 13:07:01 -0400

    Add DefaultParameterMapBuilder class

--- src/main/java/com/mcl/App.java
+++ src/main/java/com/mcl/App.java
@@ -1,10 +1,16 @@
 package com.mcl;
 
+import com.mcl.utilities.DefaultParameterMapBuilder;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Map<String, String> params = new DefaultParameterMapBuilder().getDefaultShoppingApiParameterMap();
+        System.out.println(params);
     }
 
 }
